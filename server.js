d0b4120079468d8fa22458b82439f7a9145dbecf
rlandeo <ramirolandeo@gmail.com>
2026-04-01 11:18:50 -0300

    modifique server

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ app.get('/dev', (req, res) => {
   res.send(`
     <script>
       localStorage.setItem("developer", "true");
-      alert("✅ Modo Developer habilitado para los estilos. Serás redirigido...");
-      if (window.history.length > 1) {
-        window.history.back();
-      } else {
-        window.location.href = "/";
-      }
+      alert("✅ Modo Developer habilitado para los estilos.");
     </script>
   `);
 });
@@ -31,12 +26,7 @@ app.get('/undev', (req, res) => {
   res.send(`
     <script>
       localStorage.removeItem("developer");
-      alert("🛑 Modo Developer deshabilitado. Serás redirigido...");
-      if (window.history.length > 1) {
-        window.history.back();
-      } else {
-        window.location.href = "/";
-      }
+      alert("🛑 Modo Developer deshabilitado.");
     </script>
   `);
 });
